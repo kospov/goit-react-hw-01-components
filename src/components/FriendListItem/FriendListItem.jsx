8a3f7d1c46354a145900c98b4b7b00b7900ddc53
Konstantin Popov <popov.k.g@gmail.com>
2022-07-26 22:55:31 +0300

    update task#3 - status

--- src/components/FriendListItem/FriendListItem.jsx
+++ src/components/FriendListItem/FriendListItem.jsx
@@ -1,12 +1,19 @@
-import s from './FriendListItem.module.css'
-const FriendListItem = ({ avatar, name, id}) => {
-    return (
-        <li className={s.item} key={id}>
-            <span className={s.status}></span>
-            <img className={s.avatar} src={avatar} alt={name} width="48" />
-            <p className={s.name}>{name}</p>
-        </li>
-    )
-}
+import s from './FriendListItem.module.css';
+const FriendListItem = ({ avatar, name, id, isOnline }) => {
+  return (
+    <li className={s.item} key={id}>
+      {isOnline ? (
+        <span
+          className={s.status}
+          style={{ backgroundColor: 'LimeGreen' }}
+        ></span>
+      ) : (
+        <span className={s.status}></span>
+      )}
+      <img className={s.avatar} src={avatar} alt={name} width="48" />
+      <p className={s.name}>{name}</p>
+    </li>
+  );
+};
 
 export default FriendListItem;
