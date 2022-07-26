e8a88aff59551404d973bcd0bbbf96dec38a83e8
Konstantin Popov <popov.k.g@gmail.com>
2022-07-26 18:47:17 +0300

    done task#1 - css

---
+++ src/components/App.jsx
@@ -0,0 +1,8 @@
+
+import Container from './Container/Container';
+
+export const App = () => {
+  return (
+    <Container />
+  );
+};
--- src/components/Container/Container.jsx
+++ src/components/Container/Container.jsx
@@ -1,15 +1,16 @@
+import s from './Container.module.css';
 import Profile from '../Profile/Profile';
 import user from '../data/user.json';
 import Statistics from '../Statistics/Statistics';
-import data from '../data/data.json'
-import FriendList from '../FriendList/FriendList'
-import friends from '../data/friends.json'
-import TransactionHistory from '../TransactionHistory/TransactionHistory'
-import transactions from '../data/transactions.json'
+import data from '../data/data.json';
+import FriendList from '../FriendList/FriendList';
+import friends from '../data/friends.json';
+import TransactionHistory from '../TransactionHistory/TransactionHistory';
+import transactions from '../data/transactions.json';
 
-export const App = () => {
+const Container = () => {
   return (
-    <div>
+    <div className={s.container}>
       <Profile
         username={user.username}
         tag={user.tag}
@@ -24,3 +25,5 @@ export const App = () => {
     </div>
   );
 };
+
+export default Container;
